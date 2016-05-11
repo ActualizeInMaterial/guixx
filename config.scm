ef482a6511211f005ef06bc300337c09118070b9
ActualizeInMaterial <none>
2016-05-11 20:55:52 +0300

    initial commit

---
+++ config.scm
@@ -0,0 +1,50 @@
+;; This is an operating system configuration template
+;; for a "desktop" setup with GNOME and Xfce.
+
+(use-modules (gnu) (gnu system nss))
+(use-service-modules desktop)
+(use-package-modules certs)
+
+(operating-system
+  (host-name "guixqemu")
+  (timezone "Europe/Bucharest")
+  (locale "en_US.UTF-8")
+
+  ;; Assuming /dev/sdX is the target hard disk, and "my-root"
+  ;; is the label of the target root file system.
+  (bootloader (grub-configuration (device "/dev/disk/by-id/ata-QEMU_HARDDISK_QM00001")))
+  (file-systems (cons (file-system
+                        (device "myroot")
+                        (title 'label)
+                        (mount-point "/")
+                        (type "ext4"))
+                      %base-file-systems))
+
+  (users (cons (user-account
+                (name "z")
+                (comment "")
+                (group "users")
+                (supplementary-groups '("wheel" "netdev"
+                                        "audio" "video" "kvm"))
+                (home-directory "/home/z"))
+               %base-user-accounts))
+
+  ;; This is where we specify system-wide packages.
+  (packages
+    (append (map specification->package
+                 '("tcpdump" "htop" "emacs" "gnupg" "wget" "curl" "font-dejavu"))
+            (cons* nss-certs         ;for HTTPS access
+                   %base-packages)))
+
+  ;; Add GNOME and/or Xfce---we can choose at the log-in
+  ;; screen with F1.  Use the "desktop" services, which
+  ;; include the X11 log-in service, networking with Wicd,
+  ;; and more.
+  (services (cons*
+;;    (console-keymap-service  "dvorak")
+	     ;;(gnome-desktop-service)
+                   (xfce-desktop-service)
+                   %desktop-services))
+
+  ;; Allow resolution of '.local' host names with mDNS.
+  (name-service-switch %mdns-host-lookup-nss))
---
+++ go
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#starts up the qemu vm
+
+
+#time qemu-system-x86_64 -enable-kvm -cpu host -net user -net nic,model=virtio -m 4G -drive file=my_guix.cow,discard=on -drive file="/home/zazdxscf/Downloads/guixsd-usb-install-0.10.0.x86_64-linux",media=disk,format=raw,discard=on -boot menu=on
+
+sharedfolder="/tmp/qemushared/"
+shf_tag="qsh"
+cowhdd="my_guix.cow"
+usbstickhdd="/home/zazdxscf/Downloads/"
+#usbstickhdd+="guixsd-usb-install-0.10.0.x86_64-linux"
+usbstickhdd+="37v7r5jvypqrklw8frir0q9df929s8nf-disk-image"
+#XXX: add these args for usb stick boot(and F12, 2, during boot)
+# -drive file="$usbstickhdd",media=disk,format=raw,discard=on
+
+mkdir -p -- "$sharedfolder"
+time qemu-system-x86_64 -enable-kvm -cpu host -net user -net nic,model=virtio -m 4G -drive file="$cowhdd",discard=on -boot menu=on -virtfs local,path="$sharedfolder",security_model=none,mount_tag="$shf_tag"
+#src: https://gitlab.com/rain1/guix-wiki/wikis/qemu
+
+#XXX: mount (inside guest vm) via:
+#mount -t 9p -o trans=virtio "$shf_tag" mountpointhere -oversion=9p2000.L
+#src: http://wiki.qemu.org/Documentation/9psetup#Mounting_the_shared_path
+
+#TODO: -serial stdio -vga std
+
